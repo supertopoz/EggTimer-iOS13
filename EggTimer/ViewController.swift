66ecac7898a975cc1126e6b70ee1dc4ad44597f1
supertopoz <deepsleepsounds@gmail.com>
2020-11-13 07:13:35 +0900

    added timers and stopped title

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let eggTimes = ["Soft": 1, "Medium": 2, "Hard": 3]
+    var timer: Timer?
+    
+    
+    @IBAction func hardnessPressed(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        let seconds = eggTimes[hardness]!
+        countDownTimer(countDown: seconds)
+    }
+    
+    func countDownTimer(countDown: Int){
+        self.timer?.invalidate()
+        
+        var secondsRemaining: Int? = countDown
+        
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if secondsRemaining! > 0 {
+                print ("\(secondsRemaining) seconds")
+                secondsRemaining! -= 1
+            } else {
+                self.timerStopped()
+            }
+        }
+    }
+    
+    func setTitle(){
+        titleLabel.text = "Stopped"
+    }
+    
+    func timerStopped() {
+        setTitle()
+        self.timer?.invalidate()
+    }
 
 }
