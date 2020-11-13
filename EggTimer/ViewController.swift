e664bdb8cd511a0da2249d3974c34a1676d6eee7
supertopoz <deepsleepsounds@gmail.com>
2020-11-13 13:46:20 +0900

    completed

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,30 +11,37 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
-    let eggTimes = ["Soft": 1, "Medium": 2, "Hard": 3]
+    let eggTimes = ["Soft": 10, "Medium": 2, "Hard": 3]
     var timer: Timer?
+    var secondsPassed = 0
+    var totalTime = 0
     
     
     @IBAction func hardnessPressed(_ sender: UIButton) {
         let hardness = sender.currentTitle!
-        let seconds = eggTimes[hardness]!
-        countDownTimer(countDown: seconds)
+        totalTime = eggTimes[hardness]!
+        countDownTimer()
+        titleLabel.text = hardness
     }
     
-    func countDownTimer(countDown: Int){
+    func countDownTimer(){
+        progressBar.progress = 0.0
         self.timer?.invalidate()
-        
-        var secondsRemaining: Int? = countDown
-        
         self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
-            if secondsRemaining! > 0 {
-                print ("\(secondsRemaining) seconds")
-                secondsRemaining! -= 1
+            if self.secondsPassed <  self.totalTime {
+                self.secondsPassed += 1
+                self.incrementProgress()
             } else {
                 self.timerStopped()
             }
         }
+    }
+    
+    func incrementProgress(){
+        let progress = Float(secondsPassed) / Float(totalTime)
+        progressBar.progress = progress
     }
     
     func setTitle(){
